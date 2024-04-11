d2272698f014655d605c204a29b422ac84f71074
KP <KP@PCFEIB208-89.msad.vsb.cz>
2024-04-11 17:03:21 +0200

    test

--- server/main.js
+++ server/main.js
@@ -1,18 +1,19 @@
 import { Server } from "socket.io";
 
 const io = new Server(3000, { cors: { origin: '*' } });
-
+let uzivatel = 0
 
 io.on('connection', (socket) => {
   console.log('A user connected');
-
+  console.log(socket.id);
+  io.emit("id",socket.id);
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
 
   socket.on('chat_message', (msg) => {
     console.log('message: ' + msg);
-    io.broadcast.emit('chat_message', msg);
+    io.emit('chat_message', msg);
   });
 });
 
